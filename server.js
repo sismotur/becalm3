932b89878332e355c24d80d9413f3a29da406e1e
felipesanti <fsanti@sismotur.com>
2020-04-04 18:25:28 +0200

    Add POST measures connected to PG function

--- server.js
+++ server.js
@@ -38,20 +38,35 @@ fastify.route({
     }
 });
 
+// POST data from a device
 fastify.route({
-    method: "GET",
-    url: "/",
+    method: "POST",
+    url: "/data-sensor/:id_patient",
     schema: {
-        // request needs to have a querystring with a "name" parameter
+        // request needs to have a querystring with a "id_device" parameter
         querystring: {
-            name: { type: "string" }
+            type: "object",
+            required: ["id_device"],
+            properties: {
+                id_device: {
+                    description: "The id of the device posting the measure",
+                    type: "number"
+                }
+            },
+            // with this flag other properties cannot be retrieved
+            additionalProperties: false
         },
         // the response needs to be an object with a "hello" property of type string
         response: {
-            200: {
+            201: {
                 type: "object",
                 properties: {
-                    hello: { type: "string" }
+                    code: {
+                        type: "number"
+                    },
+                    status: {
+                        type: "string"
+                    }
                 }
             }
         }
@@ -59,10 +74,26 @@ fastify.route({
     // this function is executed for every request before the handler is executed
     preHandler: async(request, reply) => {
         // e.g. check authentication
-        fastify.log.info("Called beforeHandler route /");
+        fastify.log.info(
+            "Called beforeHandler route POST /data-sensor/:id_patient"
+        );
     },
     handler: async(request, reply) => {
-        return { hello: "mundo postgres" };
+        // call the dedicated inserting PG function
+        // const request_body = request.body;
+        // const request_id_patient = request.params.id_patient;
+        // const request_id_device = request.query.id_device;
+        // console.log("REQUEST BODY:" + JSON.stringify(request_body));
+        // console.log("REQUEST ID PATIENT:" + request_id_patient);
+        // console.log("REQUEST ID DEVICE:" + request_id_device);
+        fastify.pg.query(
+            "SELECT sd.post_measures($1, $2)", [request.params.id_patient, JSON.stringify(request.body)],
+            function onResult(err, result) {
+                console.log("********************");
+                console.log("RESULT:" + JSON.stringify(result));
+                reply.code(201).send(result.rows[0].post_measures);
+            }
+        );
     }
 });
 
@@ -71,8 +102,8 @@ const start = async() => {
     try {
         await fastify.listen(3000);
         fastify.log.info(`
-                server listening on $ { fastify.server.address().port }
-                `);
+                    server listening on ${fastify.server.address().port}
+                    `);
     } catch (err) {
         fastify.log.error(err);
         process.exit(1);
